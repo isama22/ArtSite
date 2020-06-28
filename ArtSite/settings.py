82eac8d67a044025e122504967fab496fbd6acda
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-06-27 22:55:14 -0700

    config set debug collect static to 1 like in poe's heroku settings

--- ArtSite/settings.py
+++ ArtSite/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,9 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-  '/static/'
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+print (BASE_DIR, 'BASE_DIR')
+
 
 LOGIN_REDIRECT_URL = '/'
 
